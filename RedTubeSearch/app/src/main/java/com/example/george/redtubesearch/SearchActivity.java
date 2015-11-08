66d06b5b619e53562ede0a1c8058d8bcf31cadd2
gpavlovych <pavlheo@gmail.com>
2015-11-08 20:57:23 +0200

    Keyboard is hidden after search running

--- RedTubeSearch/app/src/main/java/com/example/george/redtubesearch/SearchActivity.java
+++ RedTubeSearch/app/src/main/java/com/example/george/redtubesearch/SearchActivity.java
@@ -15,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -52,6 +53,11 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
                 startActivity(intent);
             }
         });
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
         return true;
     }
 
